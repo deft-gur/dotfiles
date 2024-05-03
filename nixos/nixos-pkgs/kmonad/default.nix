9717e1fe6571deb459b84cfb770666748c5cba7f
Ziwen Wang <1308632188@qq.com>
2024-05-03 14:45:04 -0400

    [nixos] Add pkgs/ and nixos-pkgs/ dir for declaring new packages either for home-manager for system wide configuration.nix

---
+++ nixos/nixos-pkgs/kmonad/default.nix
@@ -0,0 +1,29 @@
+{ pkgs, username, ... }:
+
+let
+  kmonad = (import ../../pkgs/kmonad/derivation.nix) pkgs;
+in
+{
+  users.groups = { uinput = {}; };
+
+  services.udev.extraRules =
+    ''
+      # KMonad user access to /dev/uinput
+      KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"
+    '';
+
+  services.kmonad = {
+    enable = true;
+    configfiles = [ ../../pkgs/kmonad/config/laptop.kbd ];
+    package = kmonad;
+  };
+
+  services.xserver = {
+    xkbOptions = "compose:ralt";
+    layout = "us";
+  };
+
+  users.extraUsers.${username} = {
+    extraGroups = [ "input" "uinput" ];
+  };
+}
